62ca777c3481b63ee6758830b29b552099c590bf
Michael C. Lehn <michel.lehn@uni-ulm.de>
2024-06-17 17:08:38 +0200

    aaaand another

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -3,9 +3,6 @@ module Jekyll
     priority :highest
 
     def generate(site)
-      puts "JEKYLL_GITALK_CLIENT_ID: #{ENV['JEKYLL_GITALK_CLIENT_ID']}"
-      puts "JEKYLL_GITALK_CLIENT_SECRET: #{ENV['JEKYLL_GITALK_CLIENT_SECRET']}"
-
       site.config['gitalk'] = {
         'clientID' => ENV['JEKYLL_GITALK_CLIENT_ID'],
         'clientSecret' => ENV['JEKYLL_GITALK_CLIENT_SECRET'],
